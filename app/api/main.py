c98d5f87a1e9ee3255fb109e5c979acee32ca409
dubenikhil461 <nikhildubey461@gmail.com>
2025-09-22 01:27:54 +0530

    cors fixed

--- app/api/main.py
+++ app/api/main.py
@@ -33,7 +33,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173","https://agri-ai-frontend-bq79.vercel.app/"],  # or ["http://localhost:5173"]
+    allow_origins=["http://localhost:5173","https://agri-ai-frontend-bq79.vercel.app"],  # or ["http://localhost:5173"]
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
