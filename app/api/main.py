eecb5e96f60b412e434212ff39d1a35641a4cfbd
dubenikhil461 <nikhildubey461@gmail.com>
2025-09-23 03:08:32 +0530

    deploy

--- app/api/main.py
+++ app/api/main.py
@@ -34,7 +34,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://agriai-ebon.vercel.app/"
+        "https://agriai-ebon.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
